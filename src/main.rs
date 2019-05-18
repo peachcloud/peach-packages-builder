12a8aeaffc1aaea2f9551ebf951730e825623bcf
Michael Williams <michael.williams@enspiral.com>
2019-05-18 12:16:19 +1200

    hey it works!

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ extern crate lazy_static;
 extern crate log;
 
 use std::env;
+use std::error;
+use std::io;
+use std::path::PathBuf;
+use std::process::Command;
 
+use git2 as git;
 use hyper::rt::{self, Future};
-use hyper::{Error, Server};
 use rifling::{Constructor, Delivery, Hook};
+use snafu::{ResultExt, Snafu};
 
 lazy_static! {
     static ref PORT: u16 = env::var("PORT")
@@ -31,28 +36,78 @@ fn main() {
                 && payload["ref"] == "refs/heads/release"
             {
                 info!("Packages are released, running build process");
-                return run();
+                if let Err(error) = run() {
+                    error!("{}", error)
+                }
+                return;
             }
         }
         info!("Ignoring web hook");
     });
     handler.register(hook);
 
-    let server = Server::bind(&addr)
+    let server = hyper::Server::bind(&addr)
         .serve(handler)
-        .map_err(|e: Error| error!("Error: {:?}", e));
+        .map_err(|e: hyper::Error| error!("Error: {:?}", e));
+
+    info!("Server listening on {}", addr);
 
     rt::run(server);
 }
 
-fn run() {
-    setup();
-    update_repo();
-    build_packages();
+fn run() -> Result<(), Box<dyn error::Error>> {
+    setup_repo()?;
+    build_packages()?;
+
+    Ok(())
 }
 
-fn setup() {}
+fn setup_repo() -> Result<(), git::Error> {
+    let path = PathBuf::from("./packages");
+    let repo = if path.exists() {
+        info!("Opening repository at {}", path.display());
+        git::Repository::open(path)
+    } else {
+        let url = "git://github.com/peachcloud/peach-packages";
+        info!("Cloning repository from {} at {}", url, path.display());
+        git::Repository::clone_recurse(url, path)
+    }?;
 
-fn update_repo() {}
+    info!("git fetch origin release");
+    let mut origin_remote = repo.find_remote("origin")?;
+    origin_remote.fetch(&["release"], None, None)?;
 
-fn build_packages() {}
+    info!("git reset --hard origin/release");
+    let oid = repo.refname_to_id("refs/remotes/origin/release")?;
+    let object = repo.find_object(oid, None).unwrap();
+    repo.reset(&object, git::ResetType::Hard, None)?;
+
+    info!("git submodule update --init --recursive");
+    repo.submodules()?
+        .into_iter()
+        .try_for_each(|mut submodule| submodule.update(true, None))?;
+
+    Ok(())
+}
+
+fn build_packages() -> Result<(), Error> {
+    info!("Building packages");
+    let status = Command::new("./build.sh")
+        .current_dir("./packages")
+        .status()
+        .context(CommandFailed {})?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        BuildFailed {}.fail()
+    }
+}
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Failed to execute process: {}", source))]
+    CommandFailed { source: std::io::Error },
+    #[snafu(display("Build failed"))]
+    BuildFailed,
+}
